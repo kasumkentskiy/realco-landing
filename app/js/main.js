1c4fe00cf284f131b7bda4267c4ddb0e00d3b0c3
kasumkentskiy <maxummaksumov@yandex.ru>
2022-11-07 01:06:29 +0400

    added locations and properties sections

---
+++ app/js/main.js
@@ -0,0 +1,42 @@
+ScrollReveal().reveal('.header', {
+  reset: true,
+  // scale: 0.5,
+  distance: '100px',
+  origin: 'top',
+});
+ScrollReveal().reveal('.howitworks', {
+  reset: true,
+  // scale: 0.5,
+  distance: '100px',
+  origin: 'bottom',
+});
+ScrollReveal().reveal('.properties', {
+  reset: true,
+  // scale: 0.5,
+  distance: '-100px',
+  origin: 'right',
+});
+ScrollReveal().reveal('.locations', {
+  reset: true,
+  // scale: 0.5,
+  distance: '-100px',
+  origin: 'right',
+});
+ScrollReveal().reveal('.gallary', {
+  reset: true,
+  // scale: 0.5,
+  distance: '100px',
+  origin: 'bottom',
+});
+ScrollReveal().reveal('.map', {
+  reset: true,
+  // scale: 0.5,
+  distance: '100px',
+  origin: 'top',
+});
+ScrollReveal().reveal('.footer', {
+  reset: true,
+  // scale: 0.5,
+  distance: '-100px',
+  origin: 'right',
+});
--- gulpfile.js
+++ gulpfile.js
@@ -46,6 +46,7 @@ function scripts() {
     'node_modules/jquery/dist/jquery.js', 
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/mixitup/dist/mixitup.min.js',
+    'node_modules/scrollreveal/dist/scrollreveal.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
