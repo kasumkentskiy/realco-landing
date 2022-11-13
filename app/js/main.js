86c116b78ad85b0f9970bacd41ec9190d30d6e12
kasumkentskiy <maxummaksumov@yandex.ru>
2022-11-13 04:00:15 +0400

    added footer

--- app/js/main.js
+++ app/js/main.js
@@ -14,35 +14,41 @@ $(function () {
     distance: '100px',
     origin: 'bottom',
   });
-  ScrollReveal().reveal('.properties', {
+  ScrollReveal().reveal('.properties__content-item', {
     reset: true,
     // scale: 0.5,
     distance: '-100px',
     origin: 'right',
   });
-  ScrollReveal().reveal('.locations', {
+  ScrollReveal().reveal('.locations__items', {
     reset: true,
-    // scale: 0.5,
+    scale: 0.5,
     distance: '-100px',
     origin: 'right',
   });
-  ScrollReveal().reveal('.gallary', {
+  ScrollReveal().reveal('.slogan', {
     reset: true,
     // scale: 0.5,
     distance: '100px',
     origin: 'bottom',
   });
-  ScrollReveal().reveal('.map', {
+  ScrollReveal().reveal('.reviews', {
     reset: true,
     // scale: 0.5,
     distance: '100px',
     origin: 'top',
   });
-  ScrollReveal().reveal('.footer', {
+  ScrollReveal().reveal('.blog', {
     reset: true,
     // scale: 0.5,
     distance: '-100px',
     origin: 'right',
+  });
+  ScrollReveal().reveal('.footer', {
+    reset: true,
+    // scale: 0.5,
+    distance: '-200px',
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
   });
 
   $(".reviews__content-stars").rateYo({
